a8f2722960194c34ac63c97df501aed38ae6d046
Hamzah Khan <hmzh.khn@gmail.com>
2016-03-26 13:39:23 -0700

    Implemented local quaternion generation (for incremental rotations with ang. vel. and dt)

--- KF/calc_rotation.m
+++ KF/calc_rotation.m
@@ -1,4 +1,4 @@
-function [ Q, n ] = calc_rotation( wxL, wyL, wzL, dt )
+function [ out ] = calc_rotation( wxL, wyL, wzL, dt )
 % calc_rotation outputs a magnitude and direction (normalized quaternion) 
 % for a rotation in dt time
 
@@ -7,15 +7,18 @@ dalphaL = wxL * dt;
 dbetaL = wyL * dt;
 dgammaL = wzL * dt;
 
+axis_of_rot = [dalphaL dbetaL dgammaL];
+
 % find magnitude of angles - dtheta is angle change about the rocket axis
 dtheta = sqrt(dalphaL^2 + dbetaL^2 + dgammaL^2);
 
 % create quaternion to represent rotation
-Q = [ cos(dtheta/2) sin(dtheta/2)*dalphaL sin(dtheta/2)*dbetaL sin(dtheta/2)*dgammaL ];
+Q = [ cos(dtheta/2) sin(dtheta/2)*axis_of_rot ];
 
 % calculate norm of quaternion, then normalize quaternion
-n = quatnorm(Q);
+n = sqrt(sum(Q.^2));
 Q = Q/n;
+out = [ n Q ];
 
 end
 
